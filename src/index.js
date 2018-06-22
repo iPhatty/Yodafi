20d9ebd4c2f022977bb09ad3844a4b779e8ec5ae
Phat Ma <phat.ma@hotmail.co.uk>
2018-06-22 20:00:34 +0100

    basic layout

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Core modules
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Css
+import "./style/style.css";
+// React Redux components
+import App from './components/app';
+
+ReactDOM.render(
+    <App />
+  , document.querySelector('.root'));
