3ddad45a610235710a5c496b71a84a56f7be5444
Phat Ma <phat.ma@hotmail.co.uk>
2018-08-20 19:27:24 +0100

    implement hot-reloader

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 
-export default class App extends Component {
-
+class App extends Component {
   render() {
-    return (
-      <div>iPhatty React</div>
-    );
+    return <div>iPhatty React</div>;
   }
 }
+
+export default hot(module)(App);
