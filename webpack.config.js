c25c8411fe4bd686ee6190135ad1e8fa3d49b45a
Phat Ma <phat.ma@hotmail.co.uk>
2018-08-20 19:23:07 +0100

    implement hot-reloader

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js',
+    filename: '[name].[hash].js',
     publicPath: '/'
   },
   devServer: {
