49009ca41f5480f0dd389415658a0624c526113c
Phat Ma <phat.ma@hotmail.co.uk>
2018-10-03 13:30:00 +0100

    adds styling to header

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import styled from 'styled-components';
 
+const Title = styled.h1`
+  font-size: 1.5rem;
+  text-align: center;
+  color: ${props => props.color || '#6ab04c'};
+`;
 class App extends Component {
   render() {
-    return <div>iPhatty React</div>;
+    return <Title>Yodafi</Title>;
   }
 }
 
