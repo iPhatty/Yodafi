5236d7d860d4c5b61b2bcfa4ee1a4036555b91a4
Phat Ma <phat.ma@hotmail.co.uk>
2018-10-07 17:01:52 +0100

    adds animation

--- src/components/result.js
+++ src/components/result.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import posed from 'react-pose';
+
+const ResultMsg = posed.span({
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 }
+});
 
 const Result = ({ result }) => {
-  return <div>{result}</div>;
+  return (
+    <div className="result result--container">
+      <ResultMsg
+        className="result--content"
+        pose={result.length > 0 ? 'visible' : 'hidden'}
+      >
+        {result}
+      </ResultMsg>
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
